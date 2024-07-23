317d9b1a03660b525b6116afcc436887a470b194
Logan <ljordan@cocc.edu>
2024-07-23 14:26:40 -0700

    Add custom items & creative mode tab

--- src/main/java/net/logan/tutorialmod/TutorialMod.java
+++ src/main/java/net/logan/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.logan.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.logan.tutorialmod.item.ModCreativeModeTabs;
+import net.logan.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -16,13 +19,15 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -33,10 +38,12 @@ public class TutorialMod {
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
-
-    // Add the example block item to the building blocks tab
+    
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
