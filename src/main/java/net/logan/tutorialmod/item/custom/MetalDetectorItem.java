0673ecfe8b63dbc0b39bf36f3a7e0edb1ba7dfd4
Logan <ljordan@cocc.edu>
2024-07-24 14:06:20 -0700

    Add custom tags

--- src/main/java/net/logan/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/logan/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.logan.tutorialmod.item.custom;
 
+import net.logan.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -63,6 +64,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.GOLD_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
