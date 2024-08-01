f723f4eb528c2152c65e71586aaeb068206d3a6f
Logan <ljordan@cocc.edu>
2024-07-31 20:22:29 -0700

    Add trunk placers

--- src/main/java/net/logan/tutorialmod/TutorialMod.java
+++ src/main/java/net/logan/tutorialmod/TutorialMod.java
@@ -15,6 +15,7 @@ import net.logan.tutorialmod.screen.ModMenuTypes;
 import net.logan.tutorialmod.sound.ModSounds;
 import net.logan.tutorialmod.util.ModWoodTypes;
 import net.logan.tutorialmod.villager.ModVillagers;
+import net.logan.tutorialmod.worldgen.tree.ModTrunkPlacerTypes;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.entity.EntityRenderers;
@@ -58,6 +59,7 @@ public class TutorialMod {
         ModMenuTypes.register(modEventBus);
 
         ModRecipes.register(modEventBus);
+        ModTrunkPlacerTypes.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
